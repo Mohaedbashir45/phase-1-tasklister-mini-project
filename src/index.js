83d5306b7d597cc7c1c51d625944583a25069cb1
mohamedbashir <mohaedbashir45@gmail.com>
2024-01-04 12:25:12 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+ document.addEventListener("DOMContentLoaded", () => {
+            // your code here
+            document.querySelector('form').addEventListener('submit', (m) => {
+                m.preventDefault();
+                const newTask = document.getElementById("new-task-description").value;
+                console.log(newTask);
+                buildTask(newTask);
+            });
+        });
+
+        function buildTask(task) {
+            const li = document.createElement("li");
+            li.textContent = task;
+            const button = document.createElement("button");
+            button.addEventListener('click', handleDelete);
+            button.textContent = 'X';
+
+            li.appendChild(button);
+            const theList = document.querySelector('#tasks');
+            theList.appendChild(li);
+        }
+
+        function handleDelete(m) {
+            m.target.parentNode.remove();
+        
